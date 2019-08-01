9add8e0d7d31e6b5f07a3bcf25fb40de759dc00e
vsung <tommy558788@gmail.com>
2019-08-01 19:36:06 +0800

    add

--- p2-continuous-control/run.py
+++ p2-continuous-control/run.py
@@ -128,6 +128,22 @@ def run(args):
     # get the default brain
     brain_name = env.brain_names[0]
     brain = env.brains[brain_name]
+    # reset the environment
+    env_info = env.reset(train_mode=True)[brain_name]
+
+    # number of agents
+    num_agents = len(env_info.agents)
+    print('Number of agents:', num_agents)
+
+    # size of each action
+    action_size = brain.vector_action_space_size
+    print('Size of each action:', action_size)
+
+    # examine the state space 
+    states = env_info.vector_observations
+    state_size = states.shape[1]
+    print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+    print('The state for the first agent looks like:', states[0])
     agent = Agent(a_dim=4, s_dim=33, clip_value=1, device=device) # continuous action clip
     train(env, agent, brain_name)
     env.close()
