1ae1810105690a94b40b64038e4ca9e65afc9714
vsung <tommy558788@gmail.com>
2019-08-06 20:15:54 +0800

    add

--- p2-continuous-control/DDPG.py
+++ p2-continuous-control/DDPG.py
@@ -79,7 +79,7 @@ class ReplayBuffer(object):
         default size : 20000 of (s_t, a_t, r_t, s_t+1)
     Input : (capacity)
     """
-    def __init__(self, capacity=1e6):
+    def __init__(self, capacity=int(1e6)):
         self.capacity = capacity
         self.memory = []
         self.position = 0
